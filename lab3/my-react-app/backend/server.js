0a6e1c73aec3eb530fff10c1467b1af0e2b8e570
Vasyl <vasyl.mushchynka.oi.2024@lpnu.ua>
2026-03-15 00:00:03 +0200

    Fix way to JSON

--- lab3/my-react-app/backend/server.js
+++ lab3/my-react-app/backend/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const admin = require("firebase-admin");
-const serviceAccount = require("./serviceAccountKey.json");
+const serviceAccount = require("../serviceAccountKey.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
